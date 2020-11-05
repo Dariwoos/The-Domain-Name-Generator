ea1372099f3d15f16cb3b103ffbffbc5cd3abe33
Dariwoos <tst.info20@gmail.com>
2020-11-05 23:06:49 +0000

    new generetor

---
+++ app.js
@@ -0,0 +1,16 @@
+  let pronoun = ['the','our'];
+  let adj = ['great', 'big' ];
+  let noun = ['jogger','racoon'];
+                        
+for(let i = 0; i <= pronoun.length - 1; i++){
+                            
+        for(let j = 0; j <= adj.length -1; j++ ){
+
+            for( let z = 0; z <= noun.length -1; z++)
+
+            console.log( pronoun[i] +adj[j] + noun[z] + ".com");
+
+            
+        }
+}
+  
